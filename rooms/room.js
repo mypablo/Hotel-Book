ca32a7e70f922b5223bfad2b5f3def1e48c6c91e
mypablo <mypablo4@gmail.com>
2020-07-03 16:20:47 -0400

    js commit

---
+++ rooms/room.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",() => {
+   const $textarea = document.querySelector("#textID");
+   const $remainingChars = document.querySelector("#RemainingChars");
+   const $errorMsg = document.querySelector("#textError");
+   const MAX_ALLOWED_CHARS = 250;
+
+
+   $textarea.addEventListener("input", (e)=>{
+       const remainingChars = MAX_ALLOWED_CHARS - e.target.value.length;
+
+       if(remainingChars >= 0){
+           $remainingChars.innerHTML = remainingChars;
+           $errorMsg.classList.add("hidden");
+          }else{
+              $errorMsg.classList.remove("hidden");
+          }
+   })
+
+   $errorMsg.classList.add("hidden");
+    
+
+
+})
