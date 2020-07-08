3a3a1ee2c7f7b4a73ed62abc76484340aea73d5d
mypablo <mypablo4@gmail.com>
2020-07-07 21:15:55 -0400

    js

--- rooms/room.js
+++ rooms/room.js
@@ -4,8 +4,13 @@ document.addEventListener("DOMContentLoaded",() => {
    const $errorMsg = document.querySelector("#textError");
    const MAX_ALLOWED_CHARS = 250;
 
+   const $addedreview = document.querySelector("#added-Review");
+   const $textareaRev = document.querySelector("#textarea-Review")
+   const $submitBtn = document.querySelector("#submit-button");
+   const $form = document.getElementById("form");
 
-
+   
+//  TEXT AREA MAX CHARS && WARNING BAR
 
    $textarea.addEventListener("input", (e)=>{
        const remainingChars = MAX_ALLOWED_CHARS - e.target.value.length;
@@ -19,7 +24,29 @@ document.addEventListener("DOMContentLoaded",() => {
    })
 
    $errorMsg.classList.add("hidden");
+
+
+   // ADDED REVIEWS
+
+
+
+   
+    $submitBtn.addEventListener("click", (e)=>{
+
+        $addedreview.innerHTML = $textareaRev.value;
     
+      console.log($textareaRev.value);
+
+      
+   
+  
+   })
+   
+
+  
+} 
+)
+  
+   
 
 
-})
